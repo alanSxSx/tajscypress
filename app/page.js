8b0903d695e773c9be6e32191341f2093710eb97
Alan Barbosa <alanzinhob@hotmail.com>
2024-01-24 21:24:25 -0300

    24_01

--- app/page.js
+++ app/page.js
@@ -1,6 +1,24 @@
+'use client'
+
+import { useState } from 'react';
 
 export default function Home() {
+
+	const [count, setCount] = useState(0);
+
+	function handleClick() {
+    setCount(count + 2);
+  }
+
+
   return (
-		<h1>Teste</h1>
+		<>
+		<div className='flex flex-col p-36'>
+		<button  className='bg-blue-300 w-16'onClick={handleClick}>Add+1</button>
+		<span>{count}</span>
+		</div>
+		</>
   );
 }
+
+
---
+++ app/pageHome.cy.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Home from './page'
+
+describe('<Home />', () => {
+  beforeEach(() => {
+    // Antes de cada teste, monta o componente Home
+    cy.mount(<Home />);
+  });
+
+  it('increments count when button is clicked', () => {
+    // Captura o valor inicial do contador
+    cy.get('.flex-col span').then(($span) => {
+      const initialCount = parseInt($span.text());
+
+      // Clica no botão
+      cy.get('.bg-blue-300').click();
+
+      // Captura o novo valor do contador
+      cy.get('.flex-col span').should(($span) => {
+        const newCount = parseInt($span.text());
+
+        // Verifica se o contador foi incrementado corretamente
+        expect(newCount).to.eq(initialCount + 1);
+      });
+    });
+  });
+});
+
+describe('First Teste Cypress', () => {
+  it('Hello World dos Testes', () => {
+    expect(2).to.equal(2)
+  })
+})
+
+
